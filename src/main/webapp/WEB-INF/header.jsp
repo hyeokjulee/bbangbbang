a988cd77c65b8b6095dfdc379c32ee04fa08fdea
BestGuby <122503489+BestGuby@users.noreply.github.com>
2023-04-12 09:18:12 +0900

    리스트 쪽 수정_임시 커밋

--- src/main/webapp/WEB-INF/header.jsp
+++ src/main/webapp/WEB-INF/header.jsp
@@ -4,7 +4,6 @@
 <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%>
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@ page session="false"%>
 
 
 <html>
--- src/main/webapp/resources/js/area.js
+++ src/main/webapp/resources/js/area.js
@@ -1,30 +1,4 @@
-				function areaGangNam() {
-					var form = document.createElement("form");
-					form.action = "/store/areaList";
-					form.method = "POST";
-					
-					var hiddenField = document.createElement("input");
-					hiddenField.type = "hidden";
-					hiddenField.name = "area1";
-					hiddenField.value = "강남";
-					
-					var hiddenField2 = document.createElement("input");
-					hiddenField2.type = "hidden";
-					hiddenField2.name = "area2";
-					hiddenField2.value = "서초";
-					
-					var hiddenField3 = document.createElement("input");
-					hiddenField3.type = "hidden";
-					hiddenField3.name = "area3";
-					hiddenField3.value = "송파";
-					
-					form.appendChild(hiddenField);
-					form.appendChild(hiddenField2);
-					form.appendChild(hiddenField3);
-					
-					document.body.appendChild(form);
-					form.submit();
-				}
+				
 				
 				function areaWest() {
 					var form = document.createElement("form");
